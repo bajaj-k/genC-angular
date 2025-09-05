153a1313b1666a7b74e741251d98e6720c2782c4
bajaj-k <karabi.bajaj@cognizant.com>
2025-09-05 19:00:36 +0530

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app';
+
+
+
+bootstrapApplication(AppComponent)
+  .catch((err) => console.error(err));
+
+
